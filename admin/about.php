a864ba7602a3d9703102abcc2a0cbf32c3311c4c
Maxime Kohlhaas <maxime@atm-consulting.fr>
2014-07-05 15:56:53 +0200

    Module basic structure

---
+++ admin/about.php
@@ -0,0 +1,87 @@
+<?php
+/* <one line to give the program's name and a brief idea of what it does.>
+ * Copyright (C) 2013 ATM Consulting <support@atm-consulting.fr>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * 	\file		admin/about.php
+ * 	\ingroup	contactdefault
+ * 	\brief		This file is an example about page
+ * 				Put some comments here
+ */
+// Dolibarr environment
+$res = @include("../../main.inc.php"); // From htdocs directory
+if (! $res) {
+    $res = @include("../../../main.inc.php"); // From "custom" directory
+}
+
+
+// Libraries
+require_once DOL_DOCUMENT_ROOT . "/core/lib/admin.lib.php";
+require_once '../lib/contactdefault.lib.php';
+
+dol_include_once('/contactdefault/lib/php-markdown/markdown.php');
+
+
+//require_once "../class/myclass.class.php";
+// Translations
+$langs->load("contactdefault@contactdefault");
+
+// Access control
+if (! $user->admin) {
+    accessforbidden();
+}
+
+// Parameters
+$action = GETPOST('action', 'alpha');
+
+/*
+ * Actions
+ */
+
+/*
+ * View
+ */
+$page_name = "ContactDefaultAbout";
+llxHeader('', $langs->trans($page_name));
+
+// Subheader
+$linkback = '<a href="' . DOL_URL_ROOT . '/admin/modules.php">'
+    . $langs->trans("BackToModuleList") . '</a>';
+print_fiche_titre($langs->trans($page_name), $linkback);
+
+// Configuration header
+$head = contactdefaultAdminPrepareHead();
+dol_fiche_head(
+    $head,
+    'about',
+    $langs->trans("Module104170Name"),
+    0,
+    'contactdefault@contactdefault'
+);
+
+// About page goes here
+$buffer = file_get_contents(dol_buildpath('/contactdefault/README.md', 0));
+echo nl2br($buffer);
+
+echo '<br><br>',
+'<a href="' . dol_buildpath('/contactdefault/COPYING', 1) . '">',
+'<img src="' . dol_buildpath('/contactdefault/img/gplv3.png', 1) . '"/>',
+'</a>';
+
+llxFooter();
+
+$db->close();
