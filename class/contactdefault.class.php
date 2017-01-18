fe965ec8af38910a137cb8cfc0b6ef663e8f6d60
gauthier <gauthier.verdol@atm-consulting.fr>
2017-01-18 17:42:26 +0100

    FIX : Euh... tc.position ça n'existe pas avant 4.0 puis on s'en sert pas de toute façon

--- class/contactdefault.class.php
+++ class/contactdefault.class.php
@@ -124,7 +124,7 @@ class ContactDefault extends Societe
         global $conf, $langs;
 
         $tab = array();
-        $sql = "SELECT DISTINCT tc.rowid, tc.code, tc.libelle, tc.element, tc.position";
+        $sql = "SELECT DISTINCT tc.rowid, tc.code, tc.libelle, tc.element";
         $sql.= " FROM ".MAIN_DB_PREFIX."c_type_contact as tc";
         $sql.= " WHERE tc.element IN ('".implode("','", $this->TElement)."')";
         if ($activeonly == 1)
