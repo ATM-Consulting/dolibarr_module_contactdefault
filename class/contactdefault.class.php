9935073b82d676cb73f5b39e7953bd65831b0c24
atm-quentin <quentin.vial-gouteyron@atm-consulting.fr>
2017-08-22 11:47:21 +0200

    FIX sql request when version > 4

--- class/contactdefault.class.php
+++ class/contactdefault.class.php
@@ -122,9 +122,10 @@ class ContactDefault extends Societe
     function liste_type_contact($source='internal', $order='code', $option=0, $activeonly=0, $code = '')
     {
         global $conf, $langs;
-
+		$addSql = '';
+		if((float)DOL_VERSION >=4) $addSql = ",tc.position";
         $tab = array();
-        $sql = "SELECT DISTINCT tc.rowid, tc.code, tc.libelle, tc.element";
+        $sql = "SELECT DISTINCT tc.rowid, tc.code, tc.libelle, tc.element".$addSql;
         $sql.= " FROM ".MAIN_DB_PREFIX."c_type_contact as tc";
         $sql.= " WHERE tc.element IN ('".implode("','", $this->TElement)."')";
         if ($activeonly == 1)
